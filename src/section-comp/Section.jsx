135dab155285efa199a7b9044e65640bfd9eb80b
FedericoBarboza <106479285+FedericoBarboza@users.noreply.github.com>
2023-04-01 20:41:40 -0300

    Mas cambios

---
+++ src/section-comp/Section.jsx
@@ -0,0 +1,14 @@
+import './Section.css'
+
+export function Section(){
+    return(
+        <section className='sesion'>
+            <div className='div'>
+                <p className='p'>Federico Barboza</p>
+            </div>
+            <div>
+                <p>Estudio: Analista Programador</p>
+            </div>
+        </section>
+    );
+}
