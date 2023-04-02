8a9120129fa356bcd4185fb23da7360bb0b00339
FedericoBarboza <106479285+FedericoBarboza@users.noreply.github.com>
2023-04-01 22:05:05 -0300

    Pagina terminada

--- src/section-comp/Section.jsx
+++ src/section-comp/Section.jsx
@@ -2,12 +2,23 @@ import './Section.css'
 
 export function Section(){
     return(
-        <section className='sesion'>
+        <section className='section'>
             <div className='div'>
                 <p className='p'>Federico Barboza</p>
+                <div className='circle'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+                </div>
             </div>
             <div className='div'>
-                <p className='p'>Estudio: Analista Programador</p>
+            <p className='p'>Analista Programador</p>
+                <div className='circle'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-laptop" viewBox="0 0 16 16">
+                        <path d="M13.5 3a.5.5 0 0 1 .5.5V11H2V3.5a.5.5 0 0 1 .5-.5h11zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5z"/>
+                    </svg>
+                </div>                
             </div>
         </section>
     );
